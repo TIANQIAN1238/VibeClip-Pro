f1862b6100d358a326a147be47245ce717da6dc4
CKylinMC <canszj@yeah.net>
2025-02-18 14:53:33 +0800

    fix: move window now nolonger reset to panel index

--- src/composables/usePanelWindow.ts
+++ src/composables/usePanelWindow.ts
@@ -19,20 +19,25 @@ export function usePanelWindow(clipboard: ReturnType<typeof useClipboard>) {
 
     const setupWindowListeners = async (onHide: () => void) => {
         const { loadConfig } = useConfig();
+        const isBlured = ref(true);
         const listeners = await Promise.all([
             appWindow.getCurrentWindow().listen('tauri://blur', event => {
                 if (!mouseInRange.value) {
                     console.log('blur', event);
+                    isBlured.value = true;
                     onHide();
                 }
             }),
             appWindow.getCurrentWindow().listen('tauri://focus', async () => {
-                showPreview.value = true;
-                page.value = 'index';
-                await Promise.all([
-                    loadConfig(),
-                    clipboard.refresh()
-                ]);
+                if (isBlured.value) {
+                    showPreview.value = true;
+                    page.value = 'index';
+                    await Promise.all([
+                        loadConfig(),
+                        clipboard.refresh()
+                    ]);
+                    isBlured.value = false;
+                }
             })
         ]);
 
